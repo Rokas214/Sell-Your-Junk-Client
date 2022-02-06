7956f9af3bf48c86a75b8b1c9af1b5209d467629
Rokas214 <rokas.guzys@gmail.com>
2022-02-06 10:35:27 +0200

    Form for login, and saving token

---
+++ src/components/Form.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { useEffect } from 'react';
+import { useRef } from 'react';
+import { useState } from 'react';
+
+const Form = () => {
+
+    const [userInputs, setUserInputs] = useState()
+
+
+  return (
+    <div>
+        <form onSubmit={((e) => {
+            e.preventDefault()
+            
+                fetch('http://localhost:8080/v1/auth/login',{
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify(userInputs)
+            })
+            .then(res => res.json())
+            .then(data => {
+                if(!data.token){
+                    return alert("error")
+                }else{
+                    localStorage.setItem("token", data.token)
+                }
+            })
+        })} >
+            <input type="email" onChange={((e) => setUserInputs({...userInputs, email: e.target.value}))} placeholder='email'  />
+            <input type="password" onChange={((e) => setUserInputs({...userInputs, password: e.target.value}))}  placeholder='password'  />
+            <button type='submit' >Submit</button>
+            
+        </form>
+    </div>
+)
+};
+
+export default Form;
